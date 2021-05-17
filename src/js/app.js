3a2f4a610da3442db71f66179040431c87a32414
Sávio Fialho <savio.fialho@softfinanca.com>
2021-05-17 23:43:16 +0100

    menu animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,26 @@
-import Swup from 'swup';
-const swup = new Swup();
+const menuBtn = document.querySelector('.nav__link');
+const mainMenu = document.querySelector('.menu__hidden');
+
+const max = 100;
+let start = null; 
+
+const showMenu = timestamp => {
+
+    mainMenu.style.position = "fixed"
+
+    if (!start) start = timestamp;
+    let progress = timestamp - start;
+
+    let newPosition = Math.min(progress / 5, max);
+    mainMenu.style.left = newPosition + '%';
+
+    if(newPosition < max){
+        window.requestAnimationFrame(showMenu);
+    }
+
+}
+
+
+menuBtn.addEventListener('click', function(){
+    window.requestAnimationFrame(showMenu);
+});
